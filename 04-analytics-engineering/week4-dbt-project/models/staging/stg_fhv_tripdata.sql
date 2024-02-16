fde7721b51e30b74cb5e5e34524fe59f85c98cf0
landry@lnts.me <landry@lnts.me>
2024-02-16 21:20:03 +0000

    creating fhv_fact model

--- 04-analytics-engineering/week4-dbt-project/models/staging/stg_fhv_tripdata.sql
+++ 04-analytics-engineering/week4-dbt-project/models/staging/stg_fhv_tripdata.sql
@@ -1,39 +1,24 @@
-{{
-    config(
-        materialized='view'
-    )
-}}
+with 
 
-with tripdata as 
-(
-  select *,
-    row_number() over(partition by dispatching_base_num, pickup_datetime) as rn
-  from {{ source('staging','external_fhv_tripdata_all') }}
-  where dispatching_base_num is not null 
+source as (
+
+    select * from {{ source('staging', 'external_fhv_tripdata_all') }}
+
+),
+
+renamed as (
+
+    select
+        dispatching_base_num,
+        pickup_datetime,
+        dropoff_datetime,
+        pulocationid as pickup_locationid,
+        dolocationid as dropoff_locationid,
+        sr_flag as sharedride_flag,
+        affiliated_base_number
+
+    from source
+
 )
-select
-    -- identifiers
-    {{ dbt_utils.generate_surrogate_key(['dispatching_base_num', 'pickup_datetime']) }} as tripid,
-    dispatching_base_num,
-       
-    -- timestamps
-    cast(pickup_datetime as timestamp) as pickup_datetime,
-    cast(dropOff_datetime as timestamp) as dropoff_datetime,
-    
-    -- trip info
-    {{ dbt.safe_cast("PUlocationID", api.Column.translate_type("integer")) }} as pickup_locationid,
-    {{ dbt.safe_cast("DOlocationID", api.Column.translate_type("integer")) }} as dropoff_locationid,
-    {{ dbt.safe_cast("SR_Flag", api.Column.translate_type("integer")) }} as sharedride_flag,
 
-    -- payment info
-    Affiliated_base_number as affiliated_base_number
-from tripdata
-where rn = 1
-and date(pickup_datetime) <= '2019-12-31'
-
--- dbt build --select stg_fhv_tripdata --vars '{'is_test_run': 'false'}'
-{% if var('is_test_run', default=true) %}
-
-  limit 100
-
-{% endif %}
+select * from renamed
